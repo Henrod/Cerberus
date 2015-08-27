49f88560bb6c9697dd941820d09913eeea7c5037
Henrod <henriqrodrigues@gmail.com>
2015-08-26 22:20:02 -0300

    JSON finalizado

--- app/src/main/java/com/example/henrique/cerberus/ClientServerInterface.java
+++ app/src/main/java/com/example/henrique/cerberus/ClientServerInterface.java
@@ -1,10 +1,18 @@
 package com.example.henrique.cerberus;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by henrique on 26/08/15.
@@ -19,8 +27,48 @@ public class ClientServerInterface {
     //constructor
     public ClientServerInterface(){}
 
+    //returns JSON object
     public JSONObject makeHttpRequest(String url){
+        //http client helps to send and receive data
         DefaultHttpClient httpClient = new DefaultHttpClient();
+        //our requet method is post
         HttpPost httpPost = new HttpPost(url);
+
+        try{
+            //get response
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            //get the content and store it into inputstream object
+            is = httpEntity.getContent();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+
+                is.close();
+                json = sb.toString();
+
+                try {
+                    jobj = new JSONObject(json);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+
+        return jobj;
     }
 }
