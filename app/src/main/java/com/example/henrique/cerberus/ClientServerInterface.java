9b1d98a753ecb47866e46ce4debb7a5d230e76d0
Henrod <henriqrodrigues@gmail.com>
2015-08-26 21:50:37 -0300

    terceiro commit

--- app/src/main/java/com/example/henrique/cerberus/ClientServerInterface.java
+++ app/src/main/java/com/example/henrique/cerberus/ClientServerInterface.java
@@ -1,7 +1,26 @@
 package com.example.henrique.cerberus;
 
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
 /**
  * Created by henrique on 26/08/15.
  */
+
+//Helps get data from server
 public class ClientServerInterface {
+    static InputStream is = null;
+    static JSONObject jobj = null;
+    static String json = "";
+
+    //constructor
+    public ClientServerInterface(){}
+
+    public JSONObject makeHttpRequest(String url){
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url);
+    }
 }
