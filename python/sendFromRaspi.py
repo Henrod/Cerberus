5ce48fac1d42d49d31bffcaad5c3095bd17aceee
ThiagoLira <thlira15@gmail.com>
2015-11-18 17:02:08 -0200

    script acha o modo

--- python/sendFromRaspi.py
+++ python/sendFromRaspi.py
@@ -4,29 +4,21 @@ import math
 from time import sleep
 from math import sin, cos, sqrt, atan2, radians
 import pymysql.cursors
+import time,datetime
 
-
-
-
-
-
- 
 def distance_on_unit_sphere(lat1, lon1, lat2, lon2):
  
-	R = 6373.0 #earth radius
+    R = 6373.0 #earth radius
 
-	dlon = float(lon2) - float(lon1)
-	dlat = float(lat2) - float(lat1)
-	
+    dlon = float(lon2) - float(lon1)
+    dlat = float(lat2) - float(lat1)
 
+    a = sin(dlat / 2)**2 + cos(float(lat1)) * cos(float(lat2)) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-	a = sin(dlat / 2)**2 + cos(float(lat1)) * cos(float(lat2)) * sin(dlon / 2)**2
-	c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-	distance = R * c
-	return distance #distancia em metros
-
-
+    distance = R * c
+    print "Distancia eh",truncate(distance,2) 
+    return distance #distancia em metros
 
 def truncate(f, n):
     '''Truncates/pads a float f to n decimal places without rounding'''
@@ -36,87 +28,95 @@ def truncate(f, n):
 
 
 def randomLatLong(latitude,longitude): #lat e long iniciais
-	dec_lat = random.random()/1000
-	dec_lon = random.random()/1000
-	return truncate(dec_lat+latitude,6),truncate(dec_lon+longitude,6)
+    lat = float(latitude)
+    lon = float(longitude)
+    dec_lat = random.random()/1000
+    dec_lon = random.random()/1000
+    return truncate(dec_lat+lat,6),truncate(dec_lon+lon,6)
 
 
-def moveu ((lat1,lon1),(lat2,lon2)):
-		epsulon = 2 #incerteza em metros
-		if  ( abs(distance_on_unit_sphere(lat1,lon1,lat2,lon2) - epsulon) < 1.0) :
-			return False
-		else:
-			return True
+def moveu ((lat1,lon1),(lat2,lon2),modo):
+        
+        if modo == "S":
+            epsulon = 2 #incerteza em metros
+        elif modo == "M":
+            epsulon = 500 #incerteza para motoristas
+        else :
+            print "DATABASE INVALIDA"
+
+        if  ( abs(distance_on_unit_sphere(lat1,lon1,lat2,lon2) - epsulon) < abs(1.0 + epsulon)) :
+            
+            return "Nao"
+        else:
+            return "Sim"
+
+
 '''
-
 randomLatLong (latitude,longitude) devolve uma tupla da forma (latitude2,longitude2) calculada a partir de uma pequena variavao a partir de latitude,longitude
-
 a funcao moveu recebe como parametros duas tuplas de (latitude,longitude) e calcula uma aproximacao da distancia geodesica das duas. Uma aproximacao grosseira e errada mas que deve servir pra os nossos propositos. Se vc estiver lendo isso Henrique, your the boss.
-
 '''
-
-
-
-
-
-
-
-
 i = 0
-
 '''parametros iniciais'''
-latitude = 19.99
-longitude = 73.78
+longitude = -46.59
+latitude = -23.62
+posInicial = randomLatLong(latitude,longitude)
 
-
-posAtual = latitude,longitude
-
+posAtual = posInicial
 # Connect to the database
 
-
-'''
-connection = pymysql.connect(host='us-cdbr-iron-east-02.cleardb.net',
-                             user='be50af336a3134',
-                             password='324da5dd',
-                             db='heroku_34cfa57a696e63d',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-'''
-
-connection = pymysql.connect(host='localhost',
+connection = pymysql.connect(host='172.21.221.95',
                              user='root',
-                             password='12345',
+                             password='12345', #124578Thi
                              db='cerberus_db',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
 #mysql://be50af336a3134:324da5dd@us-cdbr-iron-east-02.cleardb.net/heroku_34cfa57a696e63d?reconnect=true
+id_do_raspi = 123
 
-id_do_raspi = 121314247274242
+
+
+cur = connection.cursor()
+
+
+
+querry = "SELECT `mode` FROM `users` WHERE id_rasp=%s"
+
+
+cur.execute(querry,str(id_do_raspi))
+
+
+
+for item in cur:
+    dictModo =  item
+
+
+
+modo = dictModo['mode'] #pega o valor de MODE da database
+
 
 
 while (True): #loop principal
 
-    i+=1
+    novaPos = randomLatLong(posAtual[0],posAtual[1])
 
-    #sleep(5) #roda a cada 5 segundos
+    moveuBool = moveu(posInicial,posAtual,modo) #bool que fala se moveu ou nao
     
-    
-    novaPos = randomLatLong(posAtual)
-
-    moveuBool = moveu(novaPos,posAtual) #bool que fala se moveu ou nao
-
     posAtual = novaPos #atualiza as coisas
 
+    ts = time.time()
+
+    print datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    
+    sleep(10) #roda a cada 5 segundos
 
 
     try:
         with connection.cursor() as cursor:
             # Create a new record
             
-            sql = "UPDATE `users` SET lat = %s long = %s  moveu = %s WHERE id = %s  "
-            cursor.execute(sql, (novaPos[0], novaPos[1], moveuBool ,id_do_raspi))
+            sql = "UPDATE `users` SET `lat` = %s , `long` = %s  , `moveu` = %s WHERE `id_rasp` = %s"
+            cursor.execute(sql, (str(novaPos[0]), str(novaPos[1]), moveuBool,str(id_do_raspi)))
 
 
 
@@ -126,7 +126,7 @@ while (True): #loop principal
 
         with connection.cursor() as cursor:
             # Read a single record
-            sql = "SELECT `id`, `lat`, `long` FROM `users` WHERE `id`=%s"
+            sql = "SELECT `id`, `lat`, `long` ,`moveu` FROM `users` WHERE `id_rasp`=%s"
             cursor.execute(sql, id_do_raspi)
             result = cursor.fetchone()
             print(result)
@@ -134,13 +134,6 @@ while (True): #loop principal
     finally:
         pass
 
+    
 
-
-
-    if (i==5): #tirar isso depois, eh soh pra o programa nao rodar pra sempre nos testes
-        break
-
-
-
-
-connection.close()
+#connection.close()
